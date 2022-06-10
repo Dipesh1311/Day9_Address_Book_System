98e75f6e09398b3a5f2e50dd42cdad0946759e8c
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-06-11 00:35:41 +0530

    Added implemented program of UC1 for address book system.

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------- Welcome To Address Book Program!! ----------");
+
+    }
+}
